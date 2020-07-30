ee6bb6980febd38816db526dae46bc13522e832a
Dennis Krasnov <dennis.krasnov@gmail.com>
2020-07-30 18:51:35 -0400

    move title to routes, remove build from public/

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,6 +1,10 @@
 <script>
 </script>
 
+<svelte:head>
+	<title>Dennis Krasnov - the about page</title>
+</svelte:head>
+
 <main>
 	HIII ABOUT
 </main>
--- src/routes/Portfolio.svelte
+++ src/routes/Portfolio.svelte
@@ -1,6 +1,10 @@
 <script>
 </script>
 
+<svelte:head>
+	<title>Dennis Krasnov - Experienced developer available for hire</title>
+</svelte:head>
+
 <main>
 	HIII PORTFOLIO
 </main>
