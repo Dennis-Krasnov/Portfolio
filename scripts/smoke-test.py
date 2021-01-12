9b1e2fbd4e6ac698a0d29d0c1c75933de90384da
Dennis Krasnov <dennis@krasnov.dev>
2021-01-12 14:54:57 -0500

    update smoke test

--- scripts/smoke-test.py
+++ scripts/smoke-test.py
@@ -6,4 +6,7 @@ port = os.environ.get("PORT")
 res = requests.get(f"http://localhost:{port}")
 
 if res.status_code != 200:
-  sys.exit("Status code wasn't 200")
+    sys.exit("Status code wasn't 200")
+
+if "Dennis Krasnov" not in res.text:
+    sys.exit("Content doesn't contain 'Dennis Krasnov'")
