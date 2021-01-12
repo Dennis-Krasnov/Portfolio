ba7bf406821a11cd53ffacbb14aebc519464224e
Dennis Krasnov <dennis@krasnov.dev>
2021-01-11 20:30:30 -0500

    debug CI/CD

--- scripts/build-nginx-image.sh
+++ scripts/build-nginx-image.sh
@@ -10,16 +10,7 @@ buildah config --workingdir='/app' $CONTAINER
 buildah run $CONTAINER -- rm /etc/nginx/conf.d/default.conf
 buildah copy $CONTAINER nginx.conf /etc/nginx/conf.d/portfolio.conf
 buildah copy $CONTAINER $SVELTE_BUNDLE .
-
-buildah run $CONTAINER -- ls -l /etc/nginx/conf.d/
-buildah run $CONTAINER -- ls -l /app
-
 buildah run $CONTAINER -- nginx -t || exit 1
 
-buildah containers
-buildah images
-
 buildah commit $CONTAINER portfolio-web
-buildah images
 buildah rm $CONTAINER
-buildah containers
