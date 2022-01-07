964551db984f33377aa42a80329dc90670bfac83
Dennis Krasnov <dennis@krasnov.dev>
2022-01-07 18:29:00 -0500

    port skills section

--- src/components/Skill.svelte
+++ src/components/Skill.svelte
@@ -1,5 +1,37 @@
-<script>
-    export let name;
+<script lang="ts">
+    export let image: string;
+    export let title: string;
+    export let summary: string;
 </script>
 
-{name}
+<div class="skill-container">
+    <img src="{image}" alt="{title} icon"/>
+    <h2>{title}</h2>
+    <p>{@html summary}</p>
+</div>
+
+<style>
+  img {
+    height: 48px;
+    margin: 0 0 12px 0;
+    width: 48px;
+
+    user-select: none;
+  }
+
+  h2 {
+    font-weight: 600;
+    font-size: 19px;
+    line-height: 32px;
+
+    color: #32325d;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 28px;
+
+    color: #525f7f;
+  }
+</style>
--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -1,1 +1,23 @@
-<slot/>
+<div class="skills-container">
+    <slot/>
+</div>
+
+<style>
+    .skills-container {
+        display: grid;
+
+        /* Mobile */
+        grid-template-columns: 100%;
+        grid-template-rows: repeat(6, auto);
+        gap: 26px 0;
+    }
+
+    /* Desktop */
+    @media (min-width: 668px) {
+        .skills-container {
+            grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: repeat(1, 1fr);
+            gap: 42px 26px;
+        }
+    }
+</style>
