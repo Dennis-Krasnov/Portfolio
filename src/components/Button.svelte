ba698187cf664cefb262f5de270dae94f778a6cf
Dennis Krasnov <dennis@krasnov.dev>
2022-01-07 19:01:20 -0500

    port landing section
    - Added stripe-like button component
    - Added smooth scroll
    - Added cartoon_caricature.png and dennis_krasnov_resume.pdf

---
+++ src/components/Button.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	export let href: string;
+	export let buttonRow: boolean = false;
+	export let primary: boolean = false;
+	export let targetBlank: boolean = false;
+
+	$: buttonRowClass = buttonRow ? "button-row" : "";
+	$: primarySecondaryClass = primary ? "primary" : "secondary";
+	$: targetBlankAttribute = targetBlank ? "_blank" : "_self";
+</script>
+
+<!-- Event forward on:click -->
+<a href="{href}" class="{buttonRowClass} {primarySecondaryClass}" target={targetBlankAttribute} on:click><slot/></a>
+
+<style>
+	/* Inspired by https://stripe.com */
+
+	a {
+		color: white;
+		white-space: nowrap;
+		display: inline-block;
+		height: 40px;
+		line-height: 40px;
+		padding: 0 14px;
+		-webkit-box-shadow: 0 4px 6px rgba(50,50,93,.11),0 1px 3px rgba(0,0,0,.08);
+		box-shadow: 0 4px 6px rgba(50,50,93,.11),0 1px 3px rgba(0,0,0,.08);
+		background: #fff;
+		background-color: rgb(255, 255, 255);
+		border-radius: 4px;
+		font-size: 15px;
+		font-weight: 600;
+		text-transform: uppercase;
+		letter-spacing: .025em;
+		text-decoration: none;
+		-webkit-transition: all .15s ease;
+		transition: all .15s ease;
+	}
+
+    a.button-row {
+         margin: 15px 12px 0 0;
+     }
+
+    a.primary {
+        /* logo colour */
+         background-color: #d61f35;
+     }
+
+    a.secondary {
+         background-color: #32325d;
+     }
+
+    a:hover {
+         box-shadow: 0 7px 14px rgba(50, 50, 93, .1), 0 3px 6px rgba(0, 0, 0, .08);
+         transform: translateY(-2px);
+     }
+</style>
--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -1,6 +1,91 @@
-<script>
-    export let title;
+<script lang="ts">
+    import Button from "./Button.svelte";
+
+    export let image: string;
+    export let title: string;
+    export let summary: string;
+    export let resume: string;
 </script>
 
+<div class="landing-container">
+    <div class="lhs">
+        <h1>{title}</h1>
+        <p>{@html summary}</p>
 
-{title}
+        <div class="cta">
+            <Button href="#projects" buttonRow primary>view projects</Button><Button href={resume} buttonRow targetBlank>download resume</Button>
+        </div>
+    </div>
+    <img src="{image}" alt="Landing">
+</div>
+
+<style>
+    .landing-container {
+        display: grid;
+
+        /* Mobile */
+        padding: 32px 0 80px 0; /* FIXME */
+    }
+
+    .lhs h1 {
+        font-weight: 600;
+        margin: 0 0 20px 0;
+
+        /* Mobile */
+        font-size: 34px;
+        line-height: 44px;
+
+        color: #32325d;
+        transition: all 0.25s ease;
+    }
+
+    .lhs p {
+        font-weight: 300;
+        font-size: 23px;
+        line-height: 35px;
+
+        color: #525f7f;
+    }
+
+    .lhs .cta {
+        /* Mobile */
+        margin: 0 0 0 0;
+    }
+
+    img {
+        width: 100%;
+        height: 264px;
+
+        object-fit: contain;
+        user-select: none;
+    }
+
+    /* Desktop */
+    @media (min-width: 820px) {
+        .landing-container {
+            grid-template-columns: .55fr .45fr;
+            gap: 0 40px;
+
+            padding: 64px 0 120px 0; /* FIXME */
+
+            /* Height of lhs when p spans 2 lines */
+            min-height: 264px;
+        }
+
+        .lhs h1 {
+            font-size: 42px;
+            line-height: 52px;
+        }
+
+        .lhs .cta {
+            margin: 15px 0 0 0;
+        }
+    }
+
+    /* Mobile */
+    @media (max-width: 820px) {
+        img {
+            display: none;
+        }
+    }
+</style>
