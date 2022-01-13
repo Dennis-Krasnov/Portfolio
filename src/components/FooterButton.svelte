754ead9605df0c34aea38bb5232f40782b90caba
Dennis Krasnov <dennis@krasnov.dev>
2022-01-13 15:34:06 -0500

    port footer section
    - Added email and phone buttons
    - Commented out hackerrank button
    - Increased copyright font size
    - Removed spacing between icons and copyright
    - Removed attribution

---
+++ src/components/FooterButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let url: string;
+</script>
+
+<a href="{url}" target="_blank" rel="noopener"><slot/></a>
+
+<style>
+  a {
+    margin: 0 10px;
+    padding: 15px;
+
+    color: #556;
+    transition: all .15s ease;
+  }
+
+  /* Similar hover animation to button */
+  a:hover {
+      color: #242733;
+      /* padding-bottom: 2px;  FIXME: fix text jumping when hovering bottom part */
+      transform: translateY(-2px);
+  }
+</style>
